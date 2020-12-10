8bbdc30d7c648dbbd7fdebc01f9624fd40b106a8
Pietro Cerutti <gahr@gahr.ch>
2020-12-10 11:03:45 +0000

    Really clear the MSN array

--- imap/msn.c
+++ imap/msn.c
@@ -58,6 +58,11 @@ void imap_msn_reserve(struct MSN *msn, size_t num)
  */
 void imap_msn_free(struct MSN *msn)
 {
+  struct Email **ep = NULL;
+  ARRAY_FOREACH(ep, msn)
+  {
+      *ep = NULL;
+  }
   ARRAY_FREE(msn);
 }
 
